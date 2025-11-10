0bdb282ec49962ca5fb423041db53370eec313da
rajnishprecogs <136425719+rajnishprecogs@users.noreply.github.com>
2025-11-10 13:18:51 +0000

    Update core/appHandler.js in branch Precogs-fix-176keoop

--- core/appHandler.js
+++ core/appHandler.js
@@ -6,34 +6,37 @@ var libxmljs = require("libxmljs");
 var serialize = require("node-serialize")
 const Op = db.Sequelize.Op
 
-module.exports.userSearch = function (req, res) {
-	var query = "SELECT name,id FROM Users WHERE login='" + req.body.login + "'";
-	db.sequelize.query(query, {
-		model: db.User
-	}).then(user => {
-		if (user.length) {
-			var output = {
-				user: {
-					name: user[0].name,
-					id: user[0].id
-				}
-			}
-			res.render('app/usersearch', {
-				output: output
-			})
-		} else {
-			req.flash('warning', 'User not found')
-			res.render('app/usersearch', {
-				output: null
-			})
-		}
-	}).catch(err => {
-		req.flash('danger', 'Internal Error')
-		res.render('app/usersearch', {
-			output: null
-		})
-	})
+function (req, res) {
+    // FIX: Use parameterized queries to prevent SQL injection
+    var query = "SELECT name, id FROM Users WHERE login = :login";
+    db.sequelize.query(query, {
+        replacements: { login: req.body.login }, // FIX: safely injects user input
+        model: db.User
+    }).then(user => {
+        if (user.length) {
+            var output = {
+                user: {
+                    name: user[0].name,
+                    id: user[0].id
+                }
+            }
+            res.render('app/usersearch', {
+                output: output
+            })
+        } else {
+            req.flash('warning', 'User not found')
+            res.render('app/usersearch', {
+                output: null
+            })
+        }
+    }).catch(err => {
+        req.flash('danger', 'Internal Error')
+        res.render('app/usersearch', {
+            output: null
+        })
+    })
 }
+// This fix uses Sequelize's parameterized query feature (the `replacements` option) to safely inject user input, eliminating the risk of SQL injection. All other logic remains unchanged.
 
 module.exports.ping = function (req, res) {
 	exec('ping -c 2 ' + req.body.address, function (err, stdout, stderr) {
